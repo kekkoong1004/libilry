3046fabe63a328e0486359069b91dd2abc31a612
kekkoong <kekkoong2062@gmail.com>
2021-06-05 18:56:22 +0800

    edit mogoose connection

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set('layout', 'layouts/layout')
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.once('open', () => console.log("Connected to mongoose"))
